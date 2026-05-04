cfe1e66aadd14f813f85f4fe6484a082d2b5fbd7
Troy Clark <troyclark@OKDVTLFDYLF3.local>
2026-05-04 14:13:57 +0200

    chore: spartan-ui imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/search/search').then((m) => m.Search),
+  },
+  {
+    path: 'artist/:id',
+    loadComponent: () => import('./features/artist/artist').then((m) => m.Artist),
+  },
+  {
+    path: 'album/:id',
+    loadComponent: () => import('./features/album/album').then((m) => m.Album),
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HlmSidebarImports } from '@spartan-ng/helm/sidebar';
+import { SidebarComponent } from './features/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [SidebarComponent, HlmSidebarImports, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
 })
-export class AppComponent {}
+export class App {}
