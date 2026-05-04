d31cd3f6407897be3fe0213b0df774fff30c1015
Troy Clark <troyclark@OKDVTLFDYLF3.local>
2026-05-04 09:06:24 +0200

    chore: scaffold tailwind and spartan-ui

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AppButton } from './button-demo/button-demo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AppButton],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular-assessment-troy');
---
+++ src/app/button-demo/button-demo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+
+@Component({
+  selector: 'app-button',
+  imports: [HlmButtonImports],
+  templateUrl: 'button-demo.html',
+})
+export class AppButton {}
