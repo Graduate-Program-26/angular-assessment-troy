139aa33d8e9430c6344ff3c859f3b15d78e2b776
Troy Clark <TClark@dvtsoftware.com>
2026-05-06 09:01:14 +0200

    feat: UI cleanup

--- src/app/features/playlist-creator/playlist-creator.ts
+++ src/app/features/playlist-creator/playlist-creator.ts
@@ -1,29 +1,18 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideChevronRight, lucideListMusic } from '@ng-icons/lucide';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmInputImports } from '@spartan-ng/helm/input';
+import { HlmSeparatorImports } from '@spartan-ng/helm/separator';
 import { PlaylistStore } from '../../store/playlist.store';
 
 @Component({
   selector: 'app-playlists',
   standalone: true,
-  imports: [],
-  template: `
-    <input #nameInput type="text" placeholder="Playlist name" />
-    <button (click)="create(nameInput)">Create</button>
-
-    <ul>
-      @for (playlist of store.playlists(); track playlist.id) {
-        <li
-          role="button"
-          tabindex="0"
-          (click)="navigate(playlist.id!)"
-          (keydown.enter)="navigate(playlist.id!)"
-          (keydown.space)="navigate(playlist.id!)"
-        >
-          {{ playlist.name }}
-        </li>
-      }
-    </ul>
-  `,
+  imports: [HlmButtonImports, HlmInputImports, HlmSeparatorImports, NgIcon],
+  providers: [provideIcons({ lucideChevronRight, lucideListMusic })],
+  templateUrl: './playlist-creator.html',
 })
 export class PlaylistsComponent implements OnInit {
   store = inject(PlaylistStore);
