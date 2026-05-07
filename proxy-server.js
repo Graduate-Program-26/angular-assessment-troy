47e345028189a8fc866b1227bf773d27bbf4a12a
Troy Clark <TClark@dvtsoftware.com>
2026-05-07 23:30:24 +0200

    fix: cors issue

--- proxy-server.js
+++ proxy-server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
+
+app.use(cors({
+  origin: 'https://deezerfy.onrender.com',
+}));
 
 app.use('/deezer-api', createProxyMiddleware({
   target: 'https://api.deezer.com',
